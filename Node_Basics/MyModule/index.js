88d9ff3658256bef4e66ba4b2e4c50704377b8b1
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-03-12 19:11:01 +0530

    myModule created

---
+++ Node_Basics/MyModule/index.js
@@ -0,0 +1,13 @@
+
+
+const add = (a,b) => {
+    console.log(`${a}+${b} = ${a+b}`);
+}
+const multiply = (a,b) => {
+    console.log(`${a}*${b} = ${a*b}`);
+}
+const substract = (a,b) => {
+    console.log(`${a}-${b} = ${a-b}`);
+}
+
+module.exports = {add,substract, multiply}
